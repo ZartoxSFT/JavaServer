b9c6cc67b8ed22d70c2728fa75b841c3714ad3e1
Amin - Ayoub <ayoub.nadir@etu.univlyon1.fr>
2025-02-26 17:15:07 +0100

    Update UDPSocketScanner.java

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,9 +1,10 @@
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Main {
 
-    public static void main(String args[]) throws SocketException {
-        UDPSocketScanner.scanUDPPort(1,100,"localhost");
+    public static void main(String args[]) throws SocketException,UnknownHostException {
+        UDPSocketScanner.scanUDPPorts(1,200,"127.0.0.1");
 
     }
 }
--- Server/src/UDPSocketScanner.java
+++ Server/src/UDPSocketScanner.java
@@ -1,40 +1,48 @@
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.*;
-import java.util.Scanner;
 
 public class UDPSocketScanner {
-    public static byte[] testpacket = new byte[0];
-    public static byte[] receivedtestpacket = new byte[1];
+    private static final byte[] EMPTY_PACKET = new byte[0];
 
-    public static void scanUDPPort(int StartPort, int EndPort, String hostIP)  throws SocketException {
-        for (int i = StartPort; i < EndPort;i++){
-            scanUDPPort(i, hostIP);
+    /**
+     * Méthode qui va itérer sur les ports de startPort à endPort et appeler scanUDPPort pour chaque port
+     * @param startPort
+     * @param endPort
+     * @param hostIP
+     * @throws SocketException
+     */
+    public static void scanUDPPorts(int startPort, int endPort, String hostIP) throws SocketException, UnknownHostException {
+        for (int port = startPort; port < endPort; port++) {
+            scanUDPPort(port, hostIP);
         }
-
     }
 
-    private static void scanUDPPort(int port, String host)  throws SocketException {
-        try{
-            DatagramSocket socketClient = new DatagramSocket(port);
-            InetAddress currAddress = InetAddress.getByName(host);
-            socketClient.setSoTimeout(2000);
+    /**
+     * Méthode qui va scanner un port UDP
+     * @param port
+     * @param host
+     * @throws SocketException
+     * @throws UnknownHostException
+     * @throws SocketTimeoutException
+     * @throws IOException
+     */
+    private static void scanUDPPort(int port, String host) throws SocketException, UnknownHostException {
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            InetAddress address = InetAddress.getByName(host);
+            socket.setSoTimeout(2000);
 
-            DatagramPacket sendpacket = new DatagramPacket(testpacket, testpacket.length, currAddress, port);
+            DatagramPacket packet = new DatagramPacket(EMPTY_PACKET, EMPTY_PACKET.length, address, port);
+            socket.send(packet);
 
-            socketClient.send(sendpacket);
-
-           // DatagramPacket receivepacket = new DatagramPacket(receivedtestpacket, receivedtestpacket.length);
-           // socketClient.receive(receivepacket);
-           // String reponse = new String(receivepacket.getData(),0,receivepacket.getLength());
-            System.out.println("Port libre = " + port + " sur " + host);
-            socketClient.close();
-
-
+            System.out.println("Port disponible: " + port + " sur " + host);
         } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
+
+            System.out.println("Host inconnu : " + host);
+            throw new UnknownHostException();
+        } catch (SocketTimeoutException e) {
+            System.out.println("Port indisponible: " + port + " sur " + host);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Port indisponible: " + port + " sur " + host);
         }
     }
 }
